64c17b7fedd7a1924294197328fc930a3736f5bd
tu.huynh <tu.huynh@BE-LMP13-27.net.fpt>
2019-10-31 22:04:06 +0700

    Bai 3

---
+++ learnVariable.js
@@ -0,0 +1,18 @@
+// Syntax:
+// let tenBien
+// let tenBien = 1
+
+let myNumber = 1.2 // camel case
+// console.log('myNumber: ', myNumber)
+
+myNumber = 2.45
+// console.log('myNumber: ', myNumber+1)
+
+// Toi Ten "Teo"
+let myString = 'Toi Ten "Teo"'
+console.log(myString);
+
+// Boolean | true, false
+let isRight = true
+let isWrong = false
+
---
+++ leranConstant.js
@@ -0,0 +1,4 @@
+const PI = 3.14
+const MY_NICK_NAME = 'TEO'
+
+console.log(MY_NICK_NAME);
